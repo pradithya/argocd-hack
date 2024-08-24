489e33d5fdda863516dacbd6e6a8d3b4cd69e24e
pradithya <pradithya.aria@gmail.com>
2024-08-24 11:16:23 +0800

    Add k3d-cluster-1

--- bootstrap/argocd/register-cluster.sh
+++ bootstrap/argocd/register-cluster.sh
@@ -18,6 +18,10 @@ docker network connect k3d-cluster-1 k3d-cluster-2-server-0
 docker network connect k3d-cluster-1 k3d-cluster-3-server-0
 
 # Get Token and CA of the target clusters
+kubectx k3d-cluster-1
+CLUSTER_1_TOKEN=$(kubectl get secret $(kubectl get secret -n kube-system | grep argocd | awk '{print $1}') -n kube-system -o json | jq  -r .data.token | base64 -d)
+CLUSTER_1_CA=$(k3d kubeconfig get cluster-1 | yq '.clusters[0].cluster.certificate-authority-data')
+echo $CLUSTER_1_CA
 kubectx k3d-cluster-2
 CLUSTER_2_TOKEN=$(kubectl get secret $(kubectl get secret -n kube-system | grep argocd | awk '{print $1}') -n kube-system -o json | jq  -r .data.token | base64 -d)
 CLUSTER_2_CA=$(k3d kubeconfig get cluster-2 | yq '.clusters[0].cluster.certificate-authority-data')
@@ -30,6 +34,28 @@ echo $CLUSTER_3_CA
 
 # Create the cluster credentials in the ArgoCD control plane based on the token and CA of the target clusters
 kubectx k3d-cluster-1
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  namespace: argocd
+  name: k3d-cluster-1-cluster-secret
+  labels:
+    argocd.argoproj.io/secret-type: cluster
+type: Opaque
+stringData:
+  name: k3d-cluster-1
+  server: "https://k3d-cluster-1-server-0:6443"
+  config: | 
+    {
+      "bearerToken": "${CLUSTER_1_TOKEN}",
+      "tlsClientConfig": { 
+        "insecure": false, 
+        "caData": "${CLUSTER_1_CA}"
+      } 
+    }
+EOF
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
